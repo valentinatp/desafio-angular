65a74d54439136333a8b4ca63ae566b63ea2a334
Valentina Toledo <valentina.toledoporma@gmail.com>
2022-01-20 20:36:43 -0300

    creacion de componentes

--- poke-dex/src/app/app.module.ts
+++ poke-dex/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { DescripcionComponent } from './componentes/descripcion/descripcion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    DescripcionComponent
   ],
   imports: [
     BrowserModule,
